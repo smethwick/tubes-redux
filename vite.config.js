c2a1b44706672d498d1743fe0e0b78791354e638
leah <leah@southlondon.cc>
2023-01-28 21:56:14 +0000

    bazinga

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
+
 	plugins: [
 		sveltekit(),
 	],
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+		coverage: {
+			provider: 'istanbul' // or 'c8'
+		},
 	}
 };
 
